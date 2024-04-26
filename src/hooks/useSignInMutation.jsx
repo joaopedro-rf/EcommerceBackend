de20f03ad240d8a1d9543c005c52f6b101fc7005
Joao Pedro <joaopedro-rf@Hotmail.com>
2024-04-26 00:48:54 -0300

    ssl port change

--- src/hooks/useSignInMutation.jsx
+++ src/hooks/useSignInMutation.jsx
@@ -2,7 +2,7 @@ import { useMutation } from "@tanstack/react-query";
 
 export function useSignInMutation() {
 
-  const URL = `https://54.210.75.235:8080/api/users/login`;
+  const URL = `https://54.210.75.235:443/api/users/login`;
   const postData = async (postData) => {
     try {
       const response = await fetch(URL, {
